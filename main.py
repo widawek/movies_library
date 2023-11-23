23a5944ff40becbd45f65b680da81d5341bcce54
widawek <kolczywiewicz@gmail.com>
2023-11-23 20:51:51 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+database = [
+    {'Type': 'movie', 'Title': 'The Matrix', 'Year': 1999,
+     'Genre': 'Action', 'Counter': 0},
+    {'Type': 'movie', 'Title': 'The Godfather', 'Year': 1972,
+     'Genre': 'Drama', 'Counter': 0},
+    {'Type': 'movie', 'Title': 'Interstellar', 'Year': 2014,
+     'Genre': 'Science-Ficton', 'Counter': 0},
+    {'Type': 'series', 'Title': 'Vikings', 'Year': 2013,
+     'Genre': 'Action', 'Season': 1, 'Episode': 1, 'Counter': 0},
+    {'Type': 'series', 'Title': 'Vikings', 'Year': 2013,
+     'Genre': 'Action', 'Season': 1, 'Episode': 2, 'Counter': 0},
+    {'Type': 'series', 'Title': 'Peaky Blinders', 'Year': 2013,
+     'Genre': 'Action', 'Season': 1, 'Episode': 1, 'Counter': 0},
+    {'Type': 'series', 'Title': 'Peaky Blinders', 'Year': 2013,
+     'Genre': 'Action', 'Season': 1, 'Episode': 2, 'Counter': 0},
+    {'Type': 'series', 'Title': 'Gomorra: La serie', 'Year': 2014,
+     'Genre': 'Criminal', 'Season': 1, 'Episode': 1, 'Counter': 0},
+    {'Type': 'series', 'Title': 'Gomorra: La serie', 'Year': 2014,
+     'Genre': 'Criminal', 'Season': 1, 'Episode': 2, 'Counter': 0},
+]
+
+
+class Movie:
+    pass
